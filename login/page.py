3f7b97c25dac16e515e9ef058464a449705acb8c
Raman-RK <rtester341@gmail.com>
2024-11-22 18:38:25 +0530

    small changes

--- login/page.py
+++ login/page.py
@@ -3,15 +3,21 @@ from selenium.webdriver import ActionChains
 import pandas as pd
 from helpers.base_page import *
 import login.locators as loc
+import landing_page.locators as lpl
 
 
 class Login(CommonClass):
     bp = Base()
 
-    def click_login(self):
-        self
-        return text
+# move this function to landing page later
+    def click_login_lp(self):
+        self.click_element('ID', lpl.login_id)
 
-    def send_phone(self, text):
-        self.send_keys_to_element('CSS_SELECTOR', lo.phone_text_box, "")
-        self.send_keys_to_element('CSS_SELECTOR', lo.phone_text_box, text)
+    def send_email(self, text):
+        self.send_keys_to_element('NAME', loc.email_name, text)
+
+    def send_password(self, text):
+        self.send_keys_to_element('CSS_SELECTOR', loc.password_css, text)
+
+    def click_login(self):
+        self.click_element("XPATH", loc.login_xpath)
