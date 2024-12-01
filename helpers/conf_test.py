915a6670623e714d8ac19d161c2bef2cc7003b2e
unknown <rtester341@gmail.com>
2024-12-01 20:27:23 +0530

    dashboard locators superadmin

--- helpers/conf_test.py
+++ helpers/conf_test.py
@@ -1,28 +1,30 @@
 import pytest
+import allure
 from selenium import webdriver
+from allure_commons.types import AttachmentType
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager  # This will help manage chromedriver automatically
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def setup(request):
-    """
-    Fixture to set up a WebDriver instance for browser automation testing.
-    """
-    # Set Chrome options if needed
     options = webdriver.ChromeOptions()
-    # Optionally, you can add more options here, e.g., options.add_argument("--headless")
-
-    # Use webdriver_manager to automatically download and install chromedriver
     chromedriver_path = ChromeDriverManager().install()
-
-    # Set up the Service object with the chromedriver path
     service = Service(chromedriver_path)
-
-    # Initialize the Chrome WebDriver with the service and options
     driver = webdriver.Chrome(service=service, options=options)
-
-    # Set up WebDriver to use throughout the test session
-    request.addfinalizer(driver.quit)  # Ensure driver quits after the test session
-
+    request.addfinalizer(driver.quit)
     return driver
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get('setup')
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="screenshot_on_failure",
+                attachment_type=AttachmentType.PNG
+            )
